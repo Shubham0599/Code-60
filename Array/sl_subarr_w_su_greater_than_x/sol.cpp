42655659ef55019384465ddad79854589eea2694
Shubham0599 <b18shubham@gmail.com>
2020-12-05 14:39:22 +0530

    add

---
+++ Array/sl_subarr_w_su_greater_than_x/sol.cpp
@@ -0,0 +1,55 @@
+//wrong for the folloeing test case
+#include <iostream>
+using namespace std;
+
+int main() {
+	//code
+		int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    int sum=0;
+	    for(int i=0;i<n;i++){
+	        sum+=arr[i];
+	    }
+	    int sum1=sum,sum2=sum;
+	    int count=0;
+	    
+	    int j=1;
+	    for(int i=0;i<n;i++){
+	        if(sum1-arr[i]>x){
+	            sum1-=arr[i];
+	            count++;
+	        }
+	        else if(sum1-arr[n-j]>x){
+	            sum1-=arr[n-j];
+	            j++;
+	            count++;
+	        }
+	        else break;
+	    }
+	    int result=n-count;
+	    count=0;
+	    j=0;
+	    for(int i=n-1;i>=0;i--){
+	        if(sum2-arr[i]>x){
+	            sum2-=arr[i];
+	            count++;
+	        }
+	        else if(sum2-arr[j]>x){
+	            sum2-=arr[j];
+	            j++;
+	            count++;
+	        }
+	        else break;
+	    }
+	    int res=min(result,n-count);
+	    cout<<res<<endl;
+	}
+	return 0;
+}
