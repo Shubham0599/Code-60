7893f27187387bdc06b97176b6fff00dc233243f
Shubham0599 <b18shubham@gmail.com>
2020-12-19 22:44:23 +0530

    added naive app

--- matrix/row_with_max_1/sol.cpp
+++ matrix/row_with_max_1/sol.cpp
@@ -1,4 +1,24 @@
+//naive approch
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    // code here
+	    int res=0,i=0;
+	    int result=-1;
+	    while(i<n){
+	        int count=0;
+	        for(int j=0;j<m;j++){
+	            //count+=arr[i][j];
+	            if(arr[i][j]==1)count++;
+	        }
+	        res=max(res,count);
+	        if(res==count)result=i;
+	        i++;
+	    }
+	    return result;
+	}
 
+};
 
 //only looking at all the columns and searching for 1
 //if 1 found then the respective row have max one as they are sorted
